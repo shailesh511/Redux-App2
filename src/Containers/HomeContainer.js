dcca141abeb0f20922e4990a0b633b18e14e4d6d
shailesh511 <shaileshtripathi511@gmail.com>
2022-11-08 15:13:40 +0530

    Adding container to our app

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,3 +1,16 @@
-import Home from "../Components/Home";
+import { connect, Connect } from "react-redux";
+//Here we are connecting out reactComponent to redux
 
-export default Home;
+import Home from "../Components/Home";
+import {addToCart} from '../Services/Actions/actions'
+
+const mapStateToProps = state =>({
+
+})
+
+const mapDispatchToProps = dispatch =>({
+   addToCartHandler : data => dispatch(addToCart(data))
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
